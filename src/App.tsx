de805a3d6214564144147cc4a3a215b04ead4ed3
Zakichanu <tzakaria@hotmail.fr>
2024-05-09 21:53:57 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ThreeDCardDemo } from "./components/AceternityUI/3D-CardEffect/3d-cardComponents"
+
+
+function App() {
+
+  return (
+    <>
+      <ThreeDCardDemo />
+    </>
+  )
+}
+
+export default App

7c0294386d9a2598033f03efc49c71fa5d945882
Zakichanu <tzakaria@hotmail.fr>
2024-05-11 13:33:28 +0200

    Hands on with aceternity UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { ThreeDCardDemo } from "./components/AceternityUI/3D-CardEffect/3d-cardComponents"
+import { AnimatedPinDemo } from "./components/AceternityUI/3D-Pin/3d-pinComponents"
 
 
 function App() {
 
   return (
     <>
-      <ThreeDCardDemo />
+      <AnimatedPinDemo />
     </>
   )
 }

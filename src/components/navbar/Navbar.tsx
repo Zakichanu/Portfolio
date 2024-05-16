47128e1eb6338c536d9b9d9cc7995eaa9a3ce7ea
Zakichanu <tzakaria@hotmail.fr>
2024-05-16 21:48:17 +0200

    The menu is cleaaaan

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,31 +1,36 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import './Navbar.css';
 
 export function Navbar(props: any) {
     const [isOpen, setIsOpen] = useState(props.isOpen);
     const history = useNavigate();
+    const location = useLocation();
 
     const handleMenuClick = () => {
-        setIsOpen(!isOpen);
         document.querySelectorAll('.fade-element').forEach((element) => {
             element.classList.remove('fade-in-up');
             element.classList.add('fade-out');
         });
         setTimeout(() => {
+            setIsOpen(!isOpen);
             if (isOpen) history('/');
             else history('/menu');
         }, 1500);
     };
 
+    const handleLogoClick = () => {
+        history('/');
+    };
+
     return (
         <nav className="navbar">
             <div className="navbar-left">
-                <p style={{ fontFamily: "Bold-Italic", color: "#1f458c" }} className="text-xl md:text-3xl lg:text-4xl text-black font-bold inter-var text-center">ZT</p>
+                <p onClick={handleLogoClick} style={{ fontFamily: "Kilogram", color: "#1f458c", cursor: "pointer" }} className="text-xl md:text-3xl lg:text-4xl inter-var text-center hover:animate-pulse">ZT</p>
             </div>
-            <div className="navbar-right">
+            <div className="fade-element navbar-right">
                 <button style={{ fontFamily: "Bold", color: "#11254c" }} className="text-xl md:text-3xl lg:text-4xl text-black font-bold inter-var text-center" onClick={handleMenuClick}>
-                    {isOpen ? 'X' : '☰'}
+                    {isOpen || location.pathname === "/menu" ? 'x' : '☰'}
                 </button>
             </div>
         </nav>

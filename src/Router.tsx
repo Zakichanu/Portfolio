8fca604b42a62a6202ff82d003cdc91256d3df2a
Zakichanu <tzakaria@hotmail.fr>
2024-05-18 18:31:46 +0200

    Beginning Work Section

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,6 +5,7 @@ import { Menu } from "./components/menu/Menu";
 import { Blog } from "./components/blog/Blog";
 import About from "./components/about/About";
 import { WavyBackground } from "./components/aceternityUi/bgWavy/BgWavy";
+import { Work } from "./components/work/Work";
 
 function Router() {
     // NB : organization and chargingstation are the parameters passed in the URL. Organization represents the subdomain of the tenant and the charginstation represents the charging station ID.
@@ -19,6 +20,7 @@ function Router() {
                     <Route path="/menu" element={<Menu />} />
                     <Route path="/blog" element={<Blog />} />
                     <Route path="/about" element={<About />} />
+                    <Route path="/work" element={<Work />} />
                 </Routes>
             </WavyBackground>
 

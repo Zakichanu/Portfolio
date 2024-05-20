525fe75e8b3b16e1f3c1bc7829efaf3dafc6b0c8
Zakichanu <tzakaria@hotmail.fr>
2024-05-20 14:21:26 +0200

    Finished the main page, last thing is to write both of the articles

--- src/components/blog/Blog.tsx
+++ src/components/blog/Blog.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import { WobbleCard } from "../aceternityUi/wobbleCard/WobbleCard";
+import soccerFootAlert from "../../assets/SoccerFootAlert.jpg"
+import bannerRMA from "../../assets/bannerRMA.png"
 
 export function Blog() {
   useEffect(() => {
@@ -7,57 +9,76 @@ export function Blog() {
       element.classList.add('fade-in-up');
     });
 
+    const elements = document.querySelectorAll('.h-screen');
+    elements.forEach((element) => {
+        element.classList.remove('justify-center');
+    });
+
+    return () => {
+        elements.forEach((element) => {
+            element.classList.add('justify-center');
+        });
+    };
+
   }, []);
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 max-w-7xl mx-auto w-full">
-      <WobbleCard
-        containerClassName="col-span-1 lg:col-span-2 h-full bg-pink-800 min-h-[500px] lg:min-h-[300px]"
-        className=""
-      >
-        <div className="max-w-xs">
-          <h2 className="text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
-            Gippity AI powers the entire universe
-          </h2>
-          <p className="mt-4 text-left  text-base/6 text-neutral-200">
-            With over 100,000 mothly active bot users, Gippity AI is the most
-            popular AI platform for developers.
-          </p>
-        </div>
-        <img
-          src="/linear.webp"
-          width={500}
-          height={500}
-          alt="linear demo image"
-          className="absolute -right-4 lg:-right-[40%] grayscale filter -bottom-10 object-contain rounded-2xl"
-        />
-      </WobbleCard>
-      <WobbleCard containerClassName="col-span-1 min-h-[300px]">
-        <h2 className="max-w-80  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
-          No shirt, no shoes, no weapons.
-        </h2>
-        <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
-          If someone yells “stop!”, goes limp, or taps out, the fight is over.
+    <>
+      <div style={{ fontFamily: "Bold" }} className="mx-auto mt-20">
+        <p style={{ color: "#1f458c" }} className="fade-element text-xl md:text-3xl lg:text-5xl text-black inter-var text-center">
+          My Blog
         </p>
-      </WobbleCard>
-      <WobbleCard containerClassName="col-span-1 lg:col-span-3 bg-blue-900 min-h-[500px] lg:min-h-[600px] xl:min-h-[300px]">
-        <div className="max-w-sm">
-          <h2 className="max-w-sm md:max-w-lg  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
-            Signup for blazing-fast cutting-edge state of the art Gippity AI
-            wrapper today!
+        <p style={{ color: "#11254c", fontFamily: "Bold" }} className="fade-element text-center md:text-3xl ml-4 mt-4 text-black inter-var ">
+          I write about my journey as a developer, my projects, and my thoughts on the tech industry.
+        </p>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 max-w-7xl mx-auto w-full mt-10">
+        <WobbleCard
+          containerClassName="col-span-1 lg:col-span-2 h-full bg-black min-h-[400px] lg:min-h-[300px]"
+          className=""
+        >
+          <div className="max-w-xs">
+            <h2 className="text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+              Football Alert Twitter Bot
+            </h2>
+            <p className="mt-4 text-left  text-base/6 text-neutral-200">
+              Here's a retrospective on the development of the Football Alert that I built 3 years ago. It was my first real achievement in my little coding journey.
+            </p>
+          </div>
+          <img
+            src={soccerFootAlert}
+            width={300}
+            height={300}
+            alt="linear demo image"
+            className="absolute -right-[10%] lg:-right-[1%] filter -bottom-40 md:-bottom-20 object-contain rounded-2xl"
+          />
+        </WobbleCard>
+        <WobbleCard containerClassName="col-span-1 min-h-[300px]">
+          <h2 className="max-w-80  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+            How to approach an IOT project?
           </h2>
           <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
-            With over 100,000 mothly active bot users, Gippity AI is the most
-            popular AI platform for developers.
+            In this blog post, I share my experience of working on an IOT project. I share the challenges I faced and the lessons I learned.
           </p>
-        </div>
-        <img
-          src="/linear.webp"
-          width={500}
-          height={500}
-          alt="linear demo image"
-          className="absolute -right-10 md:-right-[40%] lg:-right-[20%] -bottom-10 object-contain rounded-2xl"
-        />
-      </WobbleCard>
-    </div>
+        </WobbleCard>
+        <WobbleCard containerClassName="col-span-1 lg:col-span-3 bg-gray-900 min-h-[500px] lg:min-h-[600px] xl:min-h-[300px]">
+          <div className="max-w-sm">
+            <h2 className="max-w-sm md:max-w-lg  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+              A day in a life of a baby CTO?
+            </h2>
+            <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
+              Since 2022, I have been working on my first ever tech startup. In this blog post, I share my journey and the lessons I have learned. And I also share my thoughts of the electric vehicle industry.
+            </p>
+          </div>
+          <img
+            src={bannerRMA}
+            width={500}
+            height={500}
+            alt="linear demo image"
+            className="absolute -right-[1%] lg:-right-[1%] filter -bottom-10 md:-bottom-5 object-contain rounded-2xl"
+          />
+        </WobbleCard>
+      </div>
+    </>
+
   );
 }

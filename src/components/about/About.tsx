a961c53ec8395ca719720f44dc737abf10d9ae50
Zakichanu <tzakaria@hotmail.fr>
2024-10-15 13:05:46 +0200

    Update profile picture about section

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import profilePic from "../../assets/profile.jpg";
 import myCV from "../../assets/CV_ENG.pdf";
-import "./About.css";
 import { useTranslation } from "react-i18next";
 
 const About = () => {
@@ -16,12 +15,12 @@ const About = () => {
 
     return (
         <div className="mt-20 flex flex-col md:flex-row sm:flex-row items-center">
-            <img src={profilePic} alt="My Face" className="img-responsive fade-element rounded-3xl md:mr-10 md:ml-10 xl:mr-10 xl:ml-10" />
+            <img src={profilePic} alt="My Face" className=" w-1/3 h-1/6 fade-element rounded-3xl md:mr-10 md:ml-10 xl:mr-10 xl:ml-10" />
             <div style={{ fontFamily: "Bold" }}>
                 <p style={{ color: "#1f458c" }} className="fade-element text-xl md:text-3xl lg:text-5xl text-black inter-var text-center">
                     {t('about.title')}
                 </p>
-                <p style={{ color: "#11254c", fontFamily: "Bold" }} className="fade-element text-center md:text-3xl ml-4 mt-4 text-black inter-var ">
+                <p style={{ color: "#11254c", fontFamily: "Bold" }} className="fade-element text-left md:text-3xl ml-4 mt-4 text-black inter-var p-2 ">
                 {t('about.begginning-content')}<a style={{ fontFamily: "Bold-Italic", color: "#1f458c" }}>Zakaria TOBBAL</a>{t('about.middle-content')} <a style={{ fontFamily: "Kilogram" }}>Zakichanu</a>{t('about.end-content')}
                 </p>
                 <br />

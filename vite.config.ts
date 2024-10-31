f671a39928c34ea018a2a90084f2f1078a6bcd1f
Zakichanu <tzakaria@hotmail.fr>
2024-10-31 17:43:03 +0100

    add sitemap dynamically

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Sitemap({
+      hostname: 'https://me.zakichan.tech',
+      dynamicRoutes: [
+        '/',
+        '/menu',
+        '/blog',
+        '/about',
+        '/work',
+        '/blog/football-alert',
+        '/blog/being-a-baby-cto',
+        '/blog/approach-iot',
+        '/blog/zakichan-technologies',
+        '/blog/when-you-jump',
+        '/blog/prioritize-tasks',
+        '/blog/mapbox'
+      ]
+    })
+  ],
 })

1a697d9f7f002ffe764d86fe8cfa1b2e5bb644d7
Zakichanu <tzakaria@hotmail.fr>
2025-05-20 11:53:40 +0200

    Update about strange behavior

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -14,12 +14,13 @@ const About = () => {
     }, []);
 
     return (
-        <div className="mt-20 flex flex-col md:flex-row sm:flex-row items-center">
-            <img src={profilePic} alt="My Face" className="p-1 w-5/6 md:w-2/5 h-1/6 fade-element rounded-3xl md:mr-10 md:ml-10 xl:mr-10 xl:ml-10" />
-            <div style={{ fontFamily: "Bold" }}>
-                <p style={{ color: "#1f458c" }} className="fade-element text-xl md:text-3xl lg:text-5xl text-black inter-var text-center">
+        <>
+        <p style={{ color: "#1f458c" }} className="mt-20 fade-element text-xl lg:text-5xl text-black inter-var text-center">
                     {t('about.title')}
                 </p>
+        <div className="flex flex-col md:flex-row sm:flex-row items-center">
+            <img src={profilePic} alt="My Face" className="p-1 w-2/6 md:w-2/5 h-1/6 fade-element rounded-3xl md:mr-10 md:ml-10 xl:mr-10 xl:ml-10" />
+            <div style={{ fontFamily: "Bold" }} className="w-1/2 md:w-1/2 h-1/6">
                 <p style={{ color: "#11254c", fontFamily: "Bold" }} className="fade-element text-left md:text-3xl ml-4 mt-4 text-black inter-var p-2 ">
                 {t('about.begginning-content')}<a style={{ fontFamily: "Bold-Italic", color: "#1f458c" }}>Zakaria TOBBAL</a>{t('about.middle-content')} <a style={{ fontFamily: "Kilogram" }}>Zakichanu</a>{t('about.end-content')}
                 </p>
@@ -29,6 +30,7 @@ const About = () => {
                 </div>
             </div>
         </div>
+        </>
     );
 }
 
